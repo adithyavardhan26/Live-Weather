7b5e769461715a5c5c931406004afe726dcdfe10
Adithya <adithyayeruva26@gmail.com>
2024-05-23 22:29:42 +0530

    first commit

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "27b7587391ba7d49f08ba10dfab95096",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
